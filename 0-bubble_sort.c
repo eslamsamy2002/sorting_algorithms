68f416766b4d96ea931512ac0072360dfbe133d7
eslam <eslam@org.com>
2023-11-20 10:18:37 -0500

    mm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	if (size < 2 || size == NULL || array == NULL)
+	if (size < 2 || !size || !array)
 		return;
 	i = 0;
 
@@ -21,9 +21,9 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[k];
-				array[k] = array[k + 1];
-				array[k + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
