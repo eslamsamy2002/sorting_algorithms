e3090a0b66cdfb5ef1eff22239b98011a1ca51d6
eslam <eslam@org.com>
2023-11-20 10:14:14 -0500

    mm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ *bubble_sort - ..
+ *
+ *@array: ..
+ *@size: ...
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	if (size < 2 || size == NULL || array == NULL)
+		return;
+	i = 0;
+
+	while (i < size)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
+			}
+		}
+		i++;
+	}
+}
