f862c00ae95aa0543f7c151d8f0bcdc994204aee
Aya11ali <alyzaghlol340@gmail.com>
2023-11-21 07:54:38 -0800

    ..

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node = NULL, *temp = NULL;
+	listint_t *node, *temp;
 
 	if (list == NULL || *list == NULL || !(*list)->next == NULL)
 		return;
